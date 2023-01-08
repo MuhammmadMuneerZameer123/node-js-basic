7b805b7752efd23de9961b890b0bfd2cbff5c2cc
MuhammmadMuneerZameer123 <68227794+MuhammmadMuneerZameer123@users.noreply.github.com>
2023-01-08 22:16:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+
+const express = require('express');
+const app = express();
+
+app.get('/',function(res,request){
+    console.log('hello');
+    
+}).listen(5500)
+
+// router.use('/post',function(request,response){
+//     response.send(
+//         [{
+//             "userId": 1,
+//             "id": 1,
+//             "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//             "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//           },
+//           {
+//             "userId": 1,
+//             "id": 2,
+//             "title": "qui est esse",
+//             "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//           }]
+    
+//     ) })
+
+
+
+
+
+
+
+// const express = require("express");
+// const router = express.Router();
+
+// router.use('/chat', require('./routes/chat'));
+
+// module.exports = router;
+
+// const http = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
